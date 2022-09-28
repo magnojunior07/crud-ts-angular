7ebf4d98f0fb89cf5ee2d61bd6e597aab41e42a1
magnojunior07 <magnojunior319@gmail.com>
2022-09-28 12:33:10 -0400

    cors issue solved

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,7 @@ import { provider_routes } from "./routes/providersRoutes";
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(client_routes);
 app.use(provider_routes);
 
